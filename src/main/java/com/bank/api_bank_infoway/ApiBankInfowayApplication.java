452c82fdfe11980e00bf199fe22c5ae48359a203
pedroleandro <pedro.leandrog@gmail.com>
2022-03-06 19:07:43 -0300

    feat(general): instalação do projeto API Bank Infoway 2020
    O projeto é um code challenge proposto pela empresa infoway em 2020 para a vaga de Desenvolvedor
    Java Pleno/Sênior. Atualmente, a empresa é conhecida como Maida Health.

---
+++ src/main/java/com/bank/api_bank_infoway/ApiBankInfowayApplication.java
@@ -0,0 +1,13 @@
+package com.bank.api_bank_infoway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBankInfowayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBankInfowayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/api_bank_infoway/ApiBankInfowayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.api_bank_infoway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBankInfowayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
